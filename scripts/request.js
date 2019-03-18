d1a16016f0f32ce783c2ff237e8d930b60bb3dd7
alugocp <alugocp@aim.com>
2019-03-18 11:18:21 -0400

    Changed GET request from HTTPS origin

--- scripts/request.js
+++ scripts/request.js
@@ -1,13 +1,19 @@
 var request={
   load:function(callback){
-    var uri=encodeURIComponent("http://youtube.com/alexlugo");
+    $.get("http://youtube.com/alexlugo",null,function(text){
+      var b=text.indexOf(" subscribers");
+      var a=b;
+      while(a>0 && text[a]!='"') a--;
+      callback((text[a]=='"')?parseInt(text.substring(a+1,b)):-1);
+    },"text/html");
+    /*var uri=encodeURIComponent("http://youtube.com/alexlugo");
     $.getJSON("http://whateverorigin.org/get?url="+uri+"&callback=?",function(data){
       var text=data.contents;
       var b=text.indexOf(" subscribers");
       var a=b;
       while(a>0 && text[a]!='"') a--;
       callback((text[a]=='"')?parseInt(text.substring(a+1,b)):-1);
-    });
+    });*/
     /*$.ajax({
       type:"GET",
       url:"https://alugocp.github.io/resume",
